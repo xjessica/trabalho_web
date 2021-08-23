60f3f1b7a1e44b2cc837a4ba6d213f88aacdda4d
Marcelo Contin <marcelo.contin@bridge.ufsc.br>
2021-08-22 22:16:35 -0300

    Add Footer e inicio do body do acompanhamento. Será feito um genérico com estrutura definida que viria do back :rocket:

--- frontend/src/modules/Acompanhamento/Acompanhamento.jsx
+++ frontend/src/modules/Acompanhamento/Acompanhamento.jsx
@@ -4,6 +4,7 @@ import './Acompanhamento.css'
 import HeaderAcompanhamento from './HeaderAcompanhamento'
 import BodyAcompanhamentoCCO from './BodyAcompanhamentoCCO'
 import BodyAcompanhamentoSistemas from './BodyAcompanhamentoSistemas'
+import Footer from '../Footer/Footer'
 
 const initialState = {
     isCCO: true
@@ -21,8 +22,10 @@ export default class Acompanhamento extends React.Component {
         return (
             <div className="acompanhamento">
                 <HeaderAcompanhamento />
+                {/* Vamos fazer genérico pra construir os cards de acordo com oq vier do banco no futuro */}
                 {this.state.isCCO && <BodyAcompanhamentoCCO />}
                 {!this.state.isCCO && <BodyAcompanhamentoSistemas />}
+                <Footer />
             </div>
         )
     }
