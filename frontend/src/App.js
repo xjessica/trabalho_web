1701dfb8f4a7a45026b274e6dcda4c8018836291
felipe.losso <felipe.losso@use-move.com>
2021-08-26 00:00:32 -0300

    Rotas adicionadas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/cadastro">Cadastro</Link>
+            </li>
+            <li>
+              <Link to="/acompanhamento/computacao">Acompanhamento Computação</Link>
+            </li>
+            <li>
+              <Link to="/acompanhamento/sistemas">Acompanhamento Sistemas</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/cadastro">
+          <Cadastro />
+        </Route>
+        <Route path="/acompanhamento/computacao">
+          <AcompanhamentoComp/>
+        </Route>
+        <Route path="/acompanhamento/sistemas">
+          <AcompanhamentoSis/>
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
+function Login() {
+  return <h2>
+    Tela login
+  </h2>
+}
+
+function Cadastro() {
+  return <h2>
+    Tela cadastro
+  </h2>
+}
+
+function AcompanhamentoComp() {
+  return <h2>
+    Acompanhamento Computação
+  </h2>
+} 
+
+function AcompanhamentoSis() {
+  return <h2>
+    Acompanhamento Sistemas
+  </h2>
+} 
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router history = {history}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
